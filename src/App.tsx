a90a1c6c626d2e02cd36a373f7863a6ef0c3a822
David Sambilay <sambilay.david@gmail.com>
2024-09-20 16:54:48 -0700

    fixed tsx and added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -67,57 +67,58 @@ function App() {
   }, [])
 
   return (
-    <>
-      {score != null && (
-        <div>
-          {score}
-        </div>
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          value={guessTemp}
-          onChange={(e) => setguessTemp(e.target.value)}
-          type='number'
-          required
-        />
-        <button type='submit'>Submit</button>
-      </form>
-
-      <div>
-        <p>Guess Temperature: {guessTemp}°F</p>
-      </div>
-
-      {actualTemp !== null && (
-        <div>
-          <p>Actual Temperature: {actualTemp}°F</p>
-        </div>
-      )}
-
+    <div className='app-container'>
       {pos !== null && (
-        <div>
-          <button onClick={handleGetLocation}>
-            Get Location
-          </button>
-
-          {pos.lat} , {pos.lng} 
+        <div className='map-container'>
           <APIProvider apiKey={mapKey} onLoad={() => console.log('Maps API has loaded.')}>
-            <Map 
-              style={{width: '500px', height: '500px'}}
-              defaultCenter={pos}
-              center={pos}
-              defaultZoom={3}
-              gestureHandling={'greedy'}
-              disableDefaultUI={true}
-            >
-              <Marker position={pos} />
-            </Map>
+              <Map
+                defaultCenter={pos}
+                center={pos}
+                defaultZoom={3}
+                gestureHandling={'none'}
+                disableDefaultUI={true}
+              >
+                <Marker position={pos} />
+              </Map>
           </APIProvider>
       </div>
       )}
 
+      <div className='score-container'>
+        {actualTemp !== null && (
+          <div>
+            <p>Actual Temperature: {actualTemp}°F</p>
+          </div>
+        )}
 
-    </>
+        {score != null && (
+          <div>
+            <p>Score: {score}</p>
+          </div>
+        )}
+      </div>
+
+      <div className='form-container'>
+        <button onClick={handleGetLocation}>
+          Refresh
+        </button>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            value={guessTemp}
+            onChange={(e) => setguessTemp(e.target.value)}
+            type='number'
+            required
+          />
+          <button type='submit'>Submit</button>
+        </form>
+
+
+        <button onClick={handleGetLocation}>
+          Get Location
+        </button>
+      </div>
+    </div>
   )
 }
 
